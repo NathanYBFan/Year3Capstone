485ac42f381a49f8ad81951389bc10c87b6a22b7
romank <xt19uj@brocku.ca>
2024-01-09 08:56:05 -0500

    Burn Debuff
    - Scripts folder and ScriptableObjects folder created
    - Added new Debuff script
    - SO created for Burn debuff

---
+++ Year3CapstoneProject/Assets/Scripts/Debuff.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Debuff", menuName = "Status Effects/Debuff")]
+public class Debuff : ScriptableObject
+{
+	public string debuffName;
+	public float debuffDuration;
+	public float damageInterval; //If this debuff damages, this is how quickly  its damage should be applied
+	public int damage; //The amount of damage this debuff does per damageInterval
+	public bool shouldKill; //Should this debuff be able to kill the entity?
+}
