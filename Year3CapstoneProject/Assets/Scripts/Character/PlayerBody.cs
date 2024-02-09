7022d702c5f12de571d5ef2780d12ee92efde381
FlockOfMeese <90285111+FlockOfMeese@users.noreply.github.com>
2024-02-09 12:31:06 -0500

    Implemented Player Death SFX

--- Year3CapstoneProject/Assets/Scripts/Character/PlayerBody.cs
+++ Year3CapstoneProject/Assets/Scripts/Character/PlayerBody.cs
@@ -126,6 +126,7 @@ public class PlayerBody : MonoBehaviour
 	public void Death()
 	{
 		isDead = true;
+		DeathSound();
 		headAnim.Play("Death");
 		StartCoroutine("DestroyPlayer");
 
@@ -220,6 +221,13 @@ public class PlayerBody : MonoBehaviour
 		velocity.y = 0;
 		rb.AddForce(newForceDirection * stats.DashSpeed, ForceMode.Impulse);
 
+	}
+
+	private void DeathSound()
+	{
+		float randPitch = Random.Range(0.8f, 1.5f);
+		audioSource.pitch = randPitch;
+		audioSource.PlayOneShot(AudioManager._Instance.PlayerAudioList[2]);
 	}
 
 	public void SetMovementVector(Vector2 dir) { moveDir = dir; if (dir.x != 0 && dir.y != 0) legDir = dir; }
