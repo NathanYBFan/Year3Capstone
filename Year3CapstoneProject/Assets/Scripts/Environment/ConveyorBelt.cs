eaa7092fbfb080124a17b639b964e54293c970e9
romank <xt19uj@brocku.ca>
2024-03-01 23:30:34 -0500

    The other half of the Rolling + conveyor bug fix

--- Year3CapstoneProject/Assets/Scripts/Environment/ConveyorBelt.cs
+++ Year3CapstoneProject/Assets/Scripts/Environment/ConveyorBelt.cs
@@ -11,13 +11,30 @@ public class ConveyorBelt : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.transform.GetComponent<CapsuleCollider>() != null && other.transform.GetComponent<CapsuleCollider>().CompareTag("Player") && other.transform.parent.parent.GetComponent<Rigidbody>() != null && transform.parent.parent.GetComponent<Platform>().effectsActive == true)
+        if (other.CompareTag("Player") && other.transform.parent.parent.GetComponent<Rigidbody>() != null && transform.parent.parent.GetComponent<Platform>().effectsActive == true)
         {
             Debug.Log("Zoom");
-            other.transform.parent.parent.GetComponent<Rigidbody>().AddForce(transform.right * 5f, ForceMode.VelocityChange);
-        }
+			Vector3 velocity = other.transform.parent.parent.GetComponent<Rigidbody>().velocity;
+            PlayerBody body = other.transform.parent.parent.GetComponent<PlayerBody>();
+            if (body == null) return;
+
+            if (!body.CanMove)
+			    other.transform.parent.parent.GetComponent<Rigidbody>().AddForce((transform.right * 5f) - velocity, ForceMode.VelocityChange);
+            else
+				other.transform.parent.parent.GetComponent<Rigidbody>().AddForce((transform.right * 5f), ForceMode.VelocityChange);
+		}
 
     }
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player") && other.transform.parent.parent.GetComponent<Rigidbody>() != null && transform.parent.parent.GetComponent<Platform>().effectsActive == true)
+		{
+			PlayerBody body = other.transform.parent.parent.GetComponent<PlayerBody>();
+			if (body == null) return;
+			if (!body.CanMove)
+				other.transform.parent.parent.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		}
+	}
 
 
 }
