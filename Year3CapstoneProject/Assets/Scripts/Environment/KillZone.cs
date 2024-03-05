3b0c7cee4d91d78c4090154c2e041e0f509472e8
Nathan Fan <90417283+NathanYBFan@users.noreply.github.com>
2024-03-04 23:20:15 -0500

    Kill zone made

---
+++ Year3CapstoneProject/Assets/Scripts/Environment/KillZone.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        // Deal Damage
+        PlayerStats stats = other.transform.parent.parent.GetComponent<PlayerStats>();
+        stats.TakeDamage(9999999);
+    }
+}
