4c16ec723f91662e80e8785d235c569dbf77e9d0
romank <xt19uj@brocku.ca>
2024-03-13 19:32:01 -0400

    Furnace Bug Fix
    - Fixed a bug where furnaces didn't stop spewing fire when Subzero froze them over

--- Year3CapstoneProject/Assets/Scripts/Environment/Furnace.cs
+++ Year3CapstoneProject/Assets/Scripts/Environment/Furnace.cs
@@ -4,151 +4,163 @@ using UnityEngine;
 
 public class Furnace : MonoBehaviour
 {
-    [SerializeField]
-    private bool isOn;
+	[SerializeField]
+	private bool isOn;
 
-    [SerializeField]
-    private ParticleSystem[] flameArray;
+	[SerializeField]
+	private ParticleSystem[] flameArray;
 
-    [SerializeField]
-    private int damage;
+	[SerializeField]
+	private int damage;
 
-    [SerializeField]
-    private float burnTime;
+	[SerializeField]
+	private float burnTime;
 
-    [SerializeField]
-    private BoxCollider trigger;
+	[SerializeField]
+	private BoxCollider trigger;
 
-    [SerializeField]
-    AudioSource flameSoundSource;
+	[SerializeField]
+	AudioSource flameSoundSource;
 
-    //not to be confused with damage, this is whether or not a player has been damaged
-    private bool damaged;
+	//not to be confused with damage, this is whether or not a player has been damaged
+	private bool damaged;
 
-    //the time a block waits to re-ignite its fire
-    private float delay;
+	//the time a block waits to re-ignite its fire
+	private float delay;
 
-    //Whether or not the warning puff of flame has been shown
-    private bool warned;
+	//Whether or not the warning puff of flame has been shown
+	private bool warned;
+	private Coroutine coroutine;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        delay = UnityEngine.Random.Range(2.0f,5.0f);
+	// Start is called before the first frame update
+	void Start()
+	{
+		delay = UnityEngine.Random.Range(2.0f, 5.0f);
 
-        StartCoroutine(OnOffCycle());
-    }
+		coroutine = StartCoroutine(OnOffCycle());
+	}
 
-    // In update, handle turning the flame effect on and off visually.
-    private void Update()
-    {
-        if (isOn)
-        {
-            
-            for (int i = 0; i < flameArray.Length; i++)
-            {
-                if (!flameArray[i].isPlaying)
-                {
-                    flameArray[i].Play();
-                    FlameSound();
-                }
+	// In update, handle turning the flame effect on and off visually.
+	private void Update()
+	{
+		if (!GetComponent<Platform>().effectsActive)
+		{
+			StopAllCoroutines();
+			coroutine = null;
+		}
+		else
+		{
+			if (coroutine == null) 
+				coroutine = StartCoroutine(OnOffCycle());
+		}
+		if (isOn)
+		{
 
-            }
+			for (int i = 0; i < flameArray.Length; i++)
+			{
+				if (!flameArray[i].isPlaying)
+				{
+					flameArray[i].Play();
+					FlameSound();
+				}
 
-            StartCoroutine(OnOffCycle());
-            trigger.enabled = true;
-        }
-        else if (isOn == false)
-        {
+			}
+			if (coroutine != null)
+				coroutine = StartCoroutine(OnOffCycle());
+			trigger.enabled = true;
+		}
+		else
+		{
 
-            for (int i = 0; i < flameArray.Length; i++)
-            {
-                flameArray[i].Stop(true, ParticleSystemStopBehavior.StopEmitting);
+			for (int i = 0; i < flameArray.Length; i++)
+			{
+				flameArray[i].Stop(true, ParticleSystemStopBehavior.StopEmitting);
 
-            }
-            trigger.enabled = false;
+			}
+			trigger.enabled = false;
 
-        }
+		}
 
-    }
-
-
-    void OnTriggerStay(Collider other)
-    {
-        //if the colliding object is a bullet,  delete it if the fire is on
-        if(other.gameObject.GetComponentInChildren<CapsuleCollider>() != null && other.gameObject.GetComponentInChildren<BulletBehaviour>())
-        {
-            if (isOn)
-            {
-                BulletObjectPoolManager._Instance.ExpiredBullet(other.gameObject);
-            }
-                
-            
-        }
-        //if the colliding object is a player (check tag), try to deal damage
-        if (other.gameObject.GetComponentInChildren<CapsuleCollider>() != null && other.gameObject.GetComponentInChildren<CapsuleCollider>().CompareTag("Player"))
-        {
-            //damage the player that made contact, only if the fire is on
-            if (isOn && !damaged)
-            {
-                other.transform.parent.parent.GetComponent<PlayerStats>().TakeDamage(damage, DamageType.Hazard);
-                damaged = true;
-                StartCoroutine(AllowDamage());
-            }
-
-        }
-
-    }
-
-    //if damaged is true, wait a second and then turn it false
-    private IEnumerator AllowDamage()
-    {
-        if (damaged)
-        {
-            yield return new WaitForSeconds(1f);
-            damaged = false;
-        }
-    }
-
-    //alternates the furnace between being on for (burnTime), then off for (delay)
-    private IEnumerator OnOffCycle()
-    {
-        if (isOn)
-        {
-            yield return new WaitForSeconds(burnTime);
-            //turn off fire
-            isOn = false;
-            damaged = false;
-            warned = false;
-        }
-        yield return new WaitForSeconds(delay);
-        //turn on fire
-        for (int i = 0; i < flameArray.Length; i++)
-        {
-            if (!warned)
-            {
-                flameArray[i].Emit(1);
-
-            }
-        }
-        warned = true;
-        yield return new WaitForSeconds(1);
-        isOn = true;
+	}
 
 
+	void OnTriggerStay(Collider other)
+	{
+		if (!GetComponent<Platform>().effectsActive) return;
+		//if the colliding object is a bullet,  delete it if the fire is on
+		if (other.gameObject.GetComponentInChildren<CapsuleCollider>() != null && other.gameObject.GetComponentInChildren<BulletBehaviour>())
+		{
+			if (isOn)
+			{
+				BulletObjectPoolManager._Instance.ExpiredBullet(other.gameObject);
+			}
 
-    }
 
-    //Plays the player damage sound
-    private void FlameSound()
-    {
-        float randPitch = UnityEngine.Random.Range(0.8f, 1.5f);
-        AudioSource audioSource = AudioManager._Instance.ChooseEnvAudioSource();
-        if(audioSource != null)
-        {
-            audioSource.pitch = randPitch;
-            AudioManager._Instance.PlaySoundFX(AudioManager._Instance.EnvAudioList[1], audioSource);
-        }
-       
-    }
+		}
+		//if the colliding object is a player (check tag), try to deal damage
+		if (other.gameObject.GetComponentInChildren<CapsuleCollider>() != null && other.gameObject.GetComponentInChildren<CapsuleCollider>().CompareTag("Player"))
+		{
+			//damage the player that made contact, only if the fire is on
+			if (isOn && !damaged)
+			{
+				other.transform.parent.parent.GetComponent<PlayerStats>().TakeDamage(damage, DamageType.Hazard);
+				damaged = true;
+				StartCoroutine(AllowDamage());
+			}
+
+		}
+
+	}
+
+	//if damaged is true, wait a second and then turn it false
+	private IEnumerator AllowDamage()
+	{
+		if (damaged)
+		{
+			yield return new WaitForSeconds(1f);
+			damaged = false;
+		}
+	}
+
+	//alternates the furnace between being on for (burnTime), then off for (delay)
+	private IEnumerator OnOffCycle()
+	{
+		if (isOn)
+		{
+			yield return new WaitForSeconds(burnTime);
+			//turn off fire
+			isOn = false;
+			damaged = false;
+			warned = false;
+		}
+		yield return new WaitForSeconds(delay);
+		//turn on fire
+		for (int i = 0; i < flameArray.Length; i++)
+		{
+			if (!warned)
+			{
+				flameArray[i].Emit(1);
+
+			}
+		}
+		warned = true;
+		yield return new WaitForSeconds(1);
+		isOn = true;
+		coroutine = null;
+
+
+	}
+
+	//Plays the player damage sound
+	private void FlameSound()
+	{
+		float randPitch = UnityEngine.Random.Range(0.8f, 1.5f);
+		AudioSource audioSource = AudioManager._Instance.ChooseEnvAudioSource();
+		if (audioSource != null)
+		{
+			audioSource.pitch = randPitch;
+			AudioManager._Instance.PlaySoundFX(AudioManager._Instance.EnvAudioList[1], audioSource);
+		}
+
+	}
 }
