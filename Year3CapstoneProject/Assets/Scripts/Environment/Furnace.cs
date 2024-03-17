0064ded2dddc7415f2b08b49fa9e9e02025d7d75
romank <xt19uj@brocku.ca>
2024-03-17 12:16:15 -0400

    Furnace Fixes
    - Fixed the hitbox of the furnace flames to only apply to the flames area (if you hugged the corner of the blocks when it activated, you'd take damage when you shouldn't be)
    - Adjusted interval between fire spews to be 2 seconds more than it once was (now they can idle between 4 - 7 seconds)
    - Exposed several values in the Furnace script that can now be adjusted in the prefab

--- Year3CapstoneProject/Assets/Scripts/Environment/Furnace.cs
+++ Year3CapstoneProject/Assets/Scripts/Environment/Furnace.cs
@@ -1,47 +1,59 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Furnace : MonoBehaviour
 {
+	#region Serialize Fields
 	[SerializeField]
-	private bool isOn;
-
-	[SerializeField]
+	[Foldout("Dependencies"), Tooltip("The flame particle emitters.")]
 	private ParticleSystem[] flameArray;
 
 	[SerializeField]
-	private int damage;
+	[Foldout("Dependencies"), Tooltip("")]
+	AudioSource flameSoundSource;
 
 	[SerializeField]
-	private float burnTime;
-
-	[SerializeField]
+	[Foldout("Dependencies"), Tooltip("The damage hitbox")]
 	private BoxCollider trigger;
 
 	[SerializeField]
-	AudioSource flameSoundSource;
+	[Header("Furnace Stats")]
+	[Foldout("Dependencies"), Tooltip("How much damage the furnaces deal.")]
+	private int damage;
 
-	//not to be confused with damage, this is whether or not a player has been damaged
-	private bool damaged;
+	[SerializeField]
+	[Foldout("Dependencies"), Tooltip("The interval that damage gets applied by.\nEx. A value of 1 means damage will be applied per 1 second.")]
+	private float damageInterval = 1;
 
-	//the time a block waits to re-ignite its fire
-	private float delay;
+	[SerializeField]
+	[Foldout("Dependencies"), Tooltip("How long the fire lasts for.")]
+	private float burnTime;
 
-	private Coroutine coroutine;
+	[SerializeField]
+	[Foldout("Dependencies"), Tooltip("The min time a furnace could be inactive for.")]
+	private float minWaitTime = 4;
+
+	[SerializeField]
+	[Foldout("Dependencies"), Tooltip("The max time a furnace could be inactive for.")]
+	private float maxWaitTime = 7;
+
+	[SerializeField]
+	[Foldout("Dependencies"), Tooltip("The time between the initial warning shot and the actual fire spew.")]
+	private float timeBeforeActive = 2;
+	#endregion
+	#region Private Variables
+	private bool isOn;				// True if furnace is currently burning
+	private bool damaged;			// Has a player taken damage?
+	private float delay;			// Delay between fire spews
+	#endregion
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		coroutine = StartCoroutine(FireOn());
+		StartCoroutine(FireOn());
 	}
-
-	// In update, handle turning the flame effect on and off visually.
-	private void Update()
-	{		
-
-	}
-
 
 	void OnTriggerStay(Collider other)
 	{
@@ -68,31 +80,36 @@ public class Furnace : MonoBehaviour
 
 	}
 
-	//if damaged is true, wait a second and then turn it false
 	private IEnumerator AllowDamage()
 	{
 		if (damaged)
 		{
-			yield return new WaitForSeconds(1f);
+			// If damaged is true, wait a second and then turn it false
+			yield return new WaitForSeconds(damageInterval);
 			damaged = false;
 		}
 	}
 
-	//alternates the furnace between being on for (burnTime), then off for (delay)
 	private IEnumerator FireOn()
 	{
+		// If the blocks are inactive, wait until the block becomes active again.
 		while (!GetComponent<Platform>().effectsActive)
 			yield return null;
-		delay = Random.Range(2.0f, 5.0f);
+
+		// Waiting until we should give a warning shot.
+		delay = Random.Range(minWaitTime, maxWaitTime);
 		yield return new WaitForSeconds(delay);
-		//turn on fire
+
+		// Warning given!
 		for (int i = 0; i < flameArray.Length; i++)
 		{
 			flameArray[i].Emit(1);
 		}
 
-		yield return new WaitForSeconds(2);
+		// Waiting until we go full blast with fire.
+		yield return new WaitForSeconds(timeBeforeActive);
 
+		// BURN!
 		for (int i = 0; i < flameArray.Length; i++)
 		{
 			flameArray[i].Play();
@@ -103,6 +120,7 @@ public class Furnace : MonoBehaviour
 		trigger.enabled = true;
 		yield return new WaitForSeconds(burnTime);
 
+		// Turn off the furnace.
 		StartCoroutine(FireOff());
 		yield break;
 
@@ -117,7 +135,7 @@ public class Furnace : MonoBehaviour
 		trigger.enabled = false;
 		damaged = false;
 		isOn = false;
-		coroutine = StartCoroutine(FireOn());
+		StartCoroutine(FireOn());
 		yield break;
 	}
 
