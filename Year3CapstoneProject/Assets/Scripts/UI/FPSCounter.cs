b07be6ec22aae412063cdd2848af7c7a5aeb77b5
Nathan Fan <90417283+NathanYBFan@users.noreply.github.com>
2024-03-25 18:56:26 -0400

    FPS Counter updates every 0.5 seconds instead of every frame

--- Year3CapstoneProject/Assets/Scripts/UI/FPSCounter.cs
+++ Year3CapstoneProject/Assets/Scripts/UI/FPSCounter.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -6,15 +7,35 @@ public class FPSCounter : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI textbox;
 
+    [SerializeField]
+    private int fpsUpdateTime = 50;
+
+    private void Start()
+    {
+        StartCoroutine(FpsCounterUpdate());
+    }
+
+    private IEnumerator FpsCounterUpdate()
+    {
+        int counter = 0;
+
+        while (true)
+        {
+            counter++;
+            yield return null;
+            if (textbox.gameObject.activeSelf && counter >= fpsUpdateTime)
+            {
+                textbox.text = "FPS: " + Mathf.Round((1.0f / Time.deltaTime) / 5) * 5;
+                counter = 0;
+            }
+        }
+    }
+
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // enable/disable
         if (Input.GetKeyDown(KeyCode.P))
             textbox.gameObject.SetActive(!textbox.gameObject.activeSelf);
-
-        // Only update display if object is active
-        if (!textbox.gameObject.activeSelf) return;
-        textbox.text = "FPS: " + (1.0f / Time.deltaTime);
     }
 }
