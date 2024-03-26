294a05b2d7f2809fedd478eb4e482aaa04d9b7d0
WickhamJMGibbs <wickham.gibbs@gmail.com>
2024-03-25 22:34:22 -0400

    Fixed CF alert
    - Fixed CF alert
    - removed CF related debug messages

--- Year3CapstoneProject/Assets/Scripts/ChaosFactors/BombBelt.cs
+++ Year3CapstoneProject/Assets/Scripts/ChaosFactors/BombBelt.cs
@@ -72,7 +72,6 @@ public class BombBelt : MonoBehaviour
 	}
 	private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other);
         if (other != transform.parent && other.CompareTag("Player"))
         {
             transform.parent.GetComponent<ExplosiveTag>().swapTarget(other.transform.parent.parent.gameObject);
