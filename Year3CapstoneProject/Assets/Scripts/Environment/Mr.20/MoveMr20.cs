0e856ecb7957fa43a0a62597d3f165656c7c0187
FlockOfMeese <90285111+FlockOfMeese@users.noreply.github.com>
2024-03-25 20:53:50 -0400

    Mr. 20 rotation implemented
    His movement is finalized. Any other Mr. 20 shenanigans will probably need to be M5 work at this point. (I.E. animating, actually tying into CF, etc)

--- Year3CapstoneProject/Assets/Scripts/Environment/Mr.20/MoveMr20.cs
+++ Year3CapstoneProject/Assets/Scripts/Environment/Mr.20/MoveMr20.cs
@@ -26,6 +26,7 @@ public class MoveMr20 : MonoBehaviour
     private int targetIndex;
     private bool rotating;
     private bool returning; // whether or not he is on the return trip back to the start
+    private Vector3 rotVector;
 
     // Start is called before the first frame update
     void Start()
@@ -40,8 +41,42 @@ public class MoveMr20 : MonoBehaviour
     {
         currPos = gameObject.transform.position;
         float degrees = rotSpeed * Time.deltaTime;
-        Vector3 rotVector = new Vector3(0, degrees, 0);
+        if (!returning)
+        {
+            rotVector = new Vector3(0, degrees, 0);
+        }
+        else
+        {
+            rotVector = new Vector3(0, -degrees, 0);
+        }
         
+
+        if(targetIndex == 2)
+        {
+            if (!returning && gameObject.transform.rotation.eulerAngles.y < 180)
+            {
+                //rotate clockwise until facing 180 degrees in world space
+                gameObject.transform.Rotate(rotVector, Space.World);
+            }
+            else if(returning && gameObject.transform.rotation.eulerAngles.y > 180)
+            {
+                gameObject.transform.Rotate(rotVector, Space.World);
+            }
+        }
+        if (targetIndex == 4)
+        {
+            if (!returning && gameObject.transform.rotation.eulerAngles.y < 270)
+            {
+                //rotate clockwise until facing 180 degrees in world space
+                gameObject.transform.Rotate(rotVector, Space.World);
+            }
+        }
+
+        if(targetIndex == 0 && gameObject.transform.rotation.eulerAngles.y > 90)
+        {
+            gameObject.transform.Rotate(rotVector, Space.World);
+        }
+
         gameObject.transform.position = Vector3.MoveTowards(currPos, pointList[targetIndex].transform.position, speed * Time.deltaTime);
         
       
@@ -70,6 +105,6 @@ public class MoveMr20 : MonoBehaviour
 
     }
 
-
+    
 
 }
