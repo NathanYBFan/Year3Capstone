63002908ddff2d73c2eec3a01198baf949d9e1ca
Nathan Fan <90417283+NathanYBFan@users.noreply.github.com>
2024-04-07 19:58:39 -0400

    4 WAY TIEEEEEEEEEEEEEEE

--- Year3CapstoneProject/Assets/Scripts/Menus/Leaderboard/Leaderboard.cs
+++ Year3CapstoneProject/Assets/Scripts/Menus/Leaderboard/Leaderboard.cs
@@ -99,7 +99,10 @@ public class Leaderboard : MonoBehaviour
             playerWinOrder.Add(index); // Most points to smallest
         }
 
-        crownRootObject.SetActive(playerNum == 0 || (playerNum == 1 && PlayerStatsManager._Instance.PlayerPoints[playerWinOrder[0]] == PlayerStatsManager._Instance.PlayerPoints[playerWinOrder[1]]));
+        crownRootObject.SetActive(playerNum == 0
+            || (playerNum == 1 && PlayerStatsManager._Instance.PlayerPoints[playerWinOrder[0]] == PlayerStatsManager._Instance.PlayerPoints[playerWinOrder[1]]) 
+            || (playerNum == 2 && PlayerStatsManager._Instance.PlayerPoints[playerWinOrder[0]] == PlayerStatsManager._Instance.PlayerPoints[playerWinOrder[2]])
+            || (playerNum == 3 && PlayerStatsManager._Instance.PlayerPoints[playerWinOrder[0]] == PlayerStatsManager._Instance.PlayerPoints[playerWinOrder[3]]));
 
         int selectedNumber = playerWinOrder[playerNum];
         CharacterStatsSO characterStats = GameManager._Instance.Players[selectedNumber].GetComponent<PlayerStats>().CharacterStat;
