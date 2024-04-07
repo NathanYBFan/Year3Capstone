ad238e93c5346e5510ca9b5ec23854b4085867e5
Nathan Fan <90417283+NathanYBFan@users.noreply.github.com>
2024-04-06 22:57:48 -0400

    Working but incomplete leader board changes
    colors should assign and points + face should be correct

--- Year3CapstoneProject/Assets/Scripts/Menus/Leaderboard.cs
+++ Year3CapstoneProject/Assets/Scripts/Menus/Leaderboard.cs
@@ -1,9 +1,10 @@
 using NaughtyAttributes;
-using System.Reflection;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Leaderboard : MonoBehaviour
 {
@@ -47,20 +48,57 @@ public class Leaderboard : MonoBehaviour
     [Foldout("Dependencies")]
     private TextMeshProUGUI pointGain;
 
-    private float maxPoints;
     private GameObject player;
     // Start is called before the first frame update
     void Awake()
     {
-        maxPoints = 60;
         player = GameManager._Instance.Players[playerNum];
     }
 
     private void OnEnable()
     {
         EventSystem.current.SetSelectedGameObject(firstButton);
+        
+        List<int> playerWinOrder = new List<int>();     // Saved win order
+        List<int> localPoints = new List<int>();        // Local save of the points
+
+        for (int i = 0; i < PlayerStatsManager._Instance.PlayerPoints.Length; i++)
+            localPoints.Add(PlayerStatsManager._Instance.PlayerPoints[i]);
+
+        for (int j = 0; j < 4; j++)
+        {
+            int max = 0;    // Max saved number
+            int index = 0;  // Index max number is found
+
+            for (int i = 0; i < 4; i++) // Check the points list
+            {
+                if (localPoints[i] > max)
+                {
+                    max = localPoints[i]; // Get max number
+                    index = i;
+                }
+            }
+
+            localPoints[index] = -1;
+            playerWinOrder.Add(index); // Most points to smallest
+        }
+
+        int selectedNumber = playerWinOrder[playerNum];
+        CharacterStatsSO characterStats = GameManager._Instance.Players[selectedNumber].GetComponent<PlayerStats>().CharacterStat;
+
+        // Set text boxes
         playerName.text = player.name;
-        currPoints.text = ""+PlayerStatsManager._Instance.playerPoints[playerNum];
+        currPoints.text = PlayerStatsManager._Instance.playerPoints[selectedNumber].ToString();
+
+        // Set sprites
+        playerIcon.sprite = characterStats.characterBGSprite;
+        playerIconBg.sprite = characterStats.characterSprite;
+
+        Color colorToApply = GameManager._Instance.Players[selectedNumber].GetComponent<PlayerStats>().UIColor;
+
+        gems.color = colorToApply;
+        barBg.color = colorToApply;
+        playerIconBg.color = colorToApply;
     }
 
     public void NextButtonPressed()
