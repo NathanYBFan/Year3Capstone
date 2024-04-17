42787a8d30123c0978f14735dc130be9c8d6ad3d
Nathan Fan <90417283+NathanYBFan@users.noreply.github.com>
2024-04-17 17:44:31 -0400

    Character select micro optimization and trailer plays when idleing in main menu for 1 min

--- Year3CapstoneProject/Assets/Scripts/Menus/MainMenu.cs
+++ Year3CapstoneProject/Assets/Scripts/Menus/MainMenu.cs
@@ -1,13 +1,41 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Controls;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField]
     private GameObject firstButton;
+
+    [SerializeField]
+    private GameObject trailerObject;
+
+    [SerializeField]
+    private float timeToWaitBeforeTrailer = 5; // 30 seconds
+
+    private float counter = 0;
+
     private void Start()
     {
         EventSystem.current.SetSelectedGameObject(firstButton);
+        counter = 0;
+    }
+
+    private void Update()
+    {
+        counter += (1 * Time.deltaTime);
+        if (Input.anyKey)
+        {
+            counter = 0;
+            trailerObject.SetActive(false);
+        }
+        if (counter >= timeToWaitBeforeTrailer)
+        {
+            counter = 0;
+            if (!trailerObject.activeInHierarchy)
+                trailerObject.SetActive(true);
+        }
     }
 
     // Finds the UIAudioSource, and plays the button press sound
